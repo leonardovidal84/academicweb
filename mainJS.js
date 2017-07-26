85f8c26839065a78d13a04514426ef576da8d480
Leonardo Vidal <leonardovidal84@outlook.com>
2017-07-26 18:17:11 -0300

    Addind all the files and folders

---
+++ mainJS.js
@@ -0,0 +1,18 @@
+function onCad() {
+    var x = document.getElementById("dropdown-cad").style.display;
+    if (x == "block") {
+        document.getElementById("dropdown-cad").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-cad").style.display = "block";
+    }
+}
+function onFin() {
+    var x = document.getElementById("dropdown-fin").style.display;
+    if (x == "block") {
+        document.getElementById("dropdown-fin").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-fin").style.display = "block";
+    }
+}
